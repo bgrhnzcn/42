ec1cdb2fa26283a05280c9fa06b956eaff3e039b
bgrhnzcn <bugrahanozcan16@gmail.com>
2023-09-06 15:57:25 +0300

    42 to GitHub

---
+++ C02/ex00/ft_strcpy_main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy_main.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: buozcan <buozcan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 15:53:17 by buozcan           #+#    #+#             */
+/*   Updated: 2023/09/06 15:57:19 by buozcan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_strcpy.c"
+
+char    *ft_strcpy(char *dest, char *src);
+
+int main(void)
+{
+    char str[] = "deneme metini";
+    char dest[40];
+
+    char *a = ft_strcpy(dest, str);
+
+    printf("%s\n", dest);
+    printf("%s\n", a);
+}
