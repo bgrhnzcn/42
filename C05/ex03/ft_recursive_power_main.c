a11087a554c9fc12c881375f0c6b6e76a9bd51bc
bgrhnzcn <bugrahanozcan16@gmail.com>
2023-09-14 20:40:57 +0300

    42 to GitHub

--- C05/ex03/ft_recursive_power_main.c
+++ C05/ex03/ft_recursive_power_main.c
@@ -6,7 +6,7 @@
 /*   By: buozcan <buozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 00:53:58 by buozcan           #+#    #+#             */
-/*   Updated: 2023/09/12 00:58:53 by buozcan          ###   ########.fr       */
+/*   Updated: 2023/09/14 20:40:50 by buozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 
 int main(void)
 {
-    int x = ft_recursive_power(-4, 2);
+    int x = ft_recursive_power(0, 5);
     printf("%d\n", x);
 }
